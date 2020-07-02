216a302cac01b622f7b7004e41dc41bf2b8dd04c
olmaneuh <olmaneuh@gmail.com>
2020-07-01 21:58:35 -0600

    Add the save image functionality

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, IMAGES, configure_uploads
+
+img = UploadSet("img", IMAGES)
 
 app = Flask(__name__)
+app.config["UPLOADED_IMG_DEST"] = "uploads/img"
+
+configure_uploads(app, img)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST" and "image" in request.files:
+        img.save(request.files["image"])
+        return "Suit uploaded"
+
     return render_template("index.html")
 
 
